0b2f816154749434a49b51ef61ec741608c0de6f
Sandeep Arambhya <guptasandeep188@gmail.com>
2017-10-16 17:22:02 +0530

    Delete font-cng-hover.directive.spec.ts

--- usingAttributeDirAsPropertyInsideComp/app/directives/font-cng-hover.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FontCngHoverDirective } from './font-cng-hover.directive';
-
-describe('FontCngHoverDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FontCngHoverDirective();
-    expect(directive).toBeTruthy();
-  });
-});
