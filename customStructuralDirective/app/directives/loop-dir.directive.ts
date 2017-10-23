4af479f006155b22293dc176f1ca98ed54c8e9cc
Sandeep Arambhya <guptasandeep188@gmail.com>
2017-10-23 14:33:46 +0530

    Update loop-dir.directive.ts

--- customStructuralDirective/app/directives/loop-dir.directive.ts
+++ customStructuralDirective/app/directives/loop-dir.directive.ts
@@ -7,9 +7,10 @@ export class LoopDirDirective {
 
   constructor(private tem : TemplateRef<any>, private container : ViewContainerRef) { }
   // TemplateRef<any> - is called type casting.
+//A generic type is a generic class or interface that is parameterized over types. Here TemplateRef is generic type.
+//So we need to define its type.
 
 @Input('appLoopDir') set check(n : Number){
-	
 	for(let i=1; i<=n ; i++){
 	this.container.createEmbeddedView(this.tem);
 	}
